69d674e0be252b33e69a557f6125116efb832f20
Bekti <bektibayev.amirali@gmail.com>
2023-07-11 19:47:39 +0200

    add all data & images

---
+++ src/data/clubs/all/all_clubs.js
@@ -0,0 +1,13 @@
+import { ENGLAND_CLUBS } from '../england/england';
+import { SPAIN_CLUBS } from '../spain/spain';
+import { ITALY_CLUBS } from '../italy/italy';
+import { GERMANY_CLUBS } from '../germany/germany';
+import { FRANCE_CLUBS } from '../france/france';
+
+export const ALL_CLUBS = [
+  ...ENGLAND_CLUBS,
+  ...SPAIN_CLUBS,
+  ...ITALY_CLUBS,
+  ...GERMANY_CLUBS,
+  ...FRANCE_CLUBS,
+];
