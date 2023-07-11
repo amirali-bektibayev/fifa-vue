5a59898a0d68942ec55dd3a4392268cc8dc0b772
Bekti <bektibayev.amirali@gmail.com>
2023-07-11 21:53:42 +0200

    first setup of RandomBox

---
+++ src/components/random/RandomBox.vue
@@ -0,0 +1,26 @@
+<template>
+  <div
+    :style="{ backgroundImage: `url('${clubImage}')` }"
+    class="random__box"
+  ></div>
+</template>
+
+<script>
+export default {
+  props: ['club-image'],
+};
+</script>
+
+<style scoped>
+.random__box {
+  width: 300px;
+  height: 300px;
+  background-color: #fff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-position: center;
+  background-size: contain;
+  background-repeat: no-repeat;
+}
+</style>
